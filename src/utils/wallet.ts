0038cb6f3f7a10082849ca9cda42fb3b1d732d79
Wodann <Wodann@users.noreply.github.com>
2023-10-29 22:22:33 -0500

    fix: use correct version of ethers Signer

--- src/utils/wallet.ts
+++ src/utils/wallet.ts
@@ -1,7 +1,7 @@
-import { JsonRpcSigner } from '@ethersproject/providers';
+import { Signer } from 'ethers';
 import { ethers, network } from 'hardhat';
 
-export const impersonate = async (address: string): Promise<JsonRpcSigner> => {
+export const impersonate = async (address: string): Promise<Signer> => {
   await network.provider.request({
     method: 'hardhat_impersonateAccount',
     params: [address],
